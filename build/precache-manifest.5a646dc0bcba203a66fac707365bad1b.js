4bf115a570a2b726832bb0c2a8205d79f705f3e1
ang3lmoren8 <ang3lmoren8@outlook.com>
2020-04-24 17:40:58 -0500

    Build

---
+++ build/precache-manifest.5a646dc0bcba203a66fac707365bad1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaed2f8277c75e913eb7ef8fdf6cd458",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42e7f0639084a74b0f70",
+    "url": "/static/css/2.2831ab12.chunk.css"
+  },
+  {
+    "revision": "89157ab46de4d0ffafb3",
+    "url": "/static/css/main.0c9a0e57.chunk.css"
+  },
+  {
+    "revision": "42e7f0639084a74b0f70",
+    "url": "/static/js/2.061b4259.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.061b4259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89157ab46de4d0ffafb3",
+    "url": "/static/js/main.5a25dc75.chunk.js"
+  },
+  {
+    "revision": "a99cd904a5f210788b0f",
+    "url": "/static/js/runtime-main.d14450f8.js"
+  }
+]);
